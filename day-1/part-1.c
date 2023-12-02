2b26491a714baeb6b9a50d144d70110e5275e1f2
helloellinor <ellinorlinnea@icloud.com>
2023-12-02 00:02:09 -0500

    Edits for clarity and consistency

--- day-1/part-1.c
+++ day-1/part-1.c
@@ -4,48 +4,47 @@
 
 int main()
 {
-	FILE *input;
-	char ch;
-	
-	int answer = 0;
-	bool newline = true;
-	
-	int calibration_value[2] = {0, 0};
-	
-	input = fopen("input.txt", "r");
-	if (input == NULL) {
-		perror("Cannot open provided file");
-		return 1;
-	}
-	
-	do {
-		ch = fgetc(input);
-		// Reject any characters that are not digits
-		if ( ch >= '0' && ch <= '9') {
-			int digit = ch - '0'; // Convert ASCII character to integer
-			
-			if (newline) {
-				calibration_value[0] = digit;
-				newline = false; 
-			}
-			
-			// Continue to update the ones place until a newline is found
-			calibration_value[1] = digit; 
-		}
-		else {
-			if (ch == '\n' || ch == EOF) {
-				newline = true;
-				answer += ( calibration_value[0]*10 + calibration_value[1] );
-				printf("%d%d", calibration_value[0], calibration_value[1]);
-				printf("\n");
-			}
-		}
-	} while (ch != EOF);
-	
-	fclose(input);
-	
-	printf("The answer is: %d", answer);
-	
-	return 0;
-		
+    FILE *input;
+    char ch;
+    
+    bool newline = true;
+    int answer = 0;
+    
+    int calibration_value[2] = {0, 0};
+    
+    input = fopen("input.txt", "r");
+    if (input == NULL) {
+        perror("Cannot open provided file");
+        return 1;
+    }
+    
+    do {
+        ch = fgetc(input);
+        
+        // Reject any characters that are not digits
+        if (ch >= '0' && ch <= '9') {
+            // Convert ASCII character to integer
+            int digit = ch - '0';
+            
+            // Set tens place
+            if (newline) {
+                calibration_value[0] = digit;
+                newline = false; 
+            }
+            
+            // Continue to update the ones place until a newline is found
+            calibration_value[1] = digit; 
+        } else if (ch == '\n' || ch == EOF) {
+            newline = true;
+            answer += ( calibration_value[0]*10 + calibration_value[1] );
+            printf("%d%d", calibration_value[0], calibration_value[1]);
+            printf("\n");
+        }
+    } while (ch != EOF);
+    
+    fclose(input);
+    
+    printf("The answer is: %d", answer);
+    
+    return 0;
 }
